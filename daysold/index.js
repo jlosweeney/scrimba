ba82314b9552dab99676567ba0160829d6e5e07d
jlosweeney <jlosweeney@gmail.com>
2021-04-26 19:18:07 -0400

    Add files via upload

---
+++ daysold/index.js
@@ -0,0 +1,13 @@
+// javascript
+function calcDays() {
+  var dob = $('#dob').val();
+  var birthDate = new Date(dob);
+  var currentDate = new Date();
+  var dateDiff = Math.floor((currentDate - birthDate) / (1000*60*60*24));
+
+  if (dateDiff < 0) {
+    alert('Please enter a date in the past');
+  } else {
+    $('#results').text(dateDiff + ' Days!');
+  }
+}
